6422db88982d9654fe3478ea642fb44fb265297b
Abbas <tundeeey@gmail.com>
2023-04-24 07:43:05 +0100

    been able to display all the products image and discounted price from the API, will still add the names of the product and other details about the product, then position everything accordingly

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,5 @@
 let userName = document.getElementById("userName");
+let displayTag = document.getElementById("display-items");
 
 
 function signOut() {
@@ -13,3 +14,26 @@ userName.innerHTML = currentUser.name;
 if (!currentUser) {
     window.location.href = "index.html";
 }
+
+async function showItems() {
+    let allItems = await fetch("https://dummyjson.com/products");
+    let result = await allItems.json();
+    console.log(result);
+    result.products.forEach(element => {        
+        displayTag.innerHTML += `
+        <div class="d-flex align-items-center">
+            <button>
+                <div class="position-relative">
+                    <img src="${element.images[0]}" class="items-images"></img>
+                    <div class="position absolute">${element.discountPercentage}</div>
+                </div>
+            </button>
+        </div>
+        ` 
+    });
+}
+showItems();
+
+
+// showItems("https://dummyjson.com/products");
+// displayCountries("https://fakestoreapi.com/products");
