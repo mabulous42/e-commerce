902cdfdc9b058b9f6c5499628602d0acf8ac6f1a
Abbas <tundeeey@gmail.com>
2023-04-24 09:10:47 +0100

    added comments to explain each line and function of the code so far

--- dashboard.js
+++ dashboard.js
@@ -1,20 +1,24 @@
 let userName = document.getElementById("userName");
 let displayTag = document.getElementById("display-items");
 
-
+//function that allows user to signout of his/her account
 function signOut() {
     window.location.href = "index.html";
     localStorage.removeItem("currentUser");
 }
 
+//getting list of current user from the signin page 
 let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
+//displaying the name of the current user
 userName.innerHTML = currentUser.name;
 
+//checking if there is a current user on the site, if none, the dashboard link should not be accessible, it should redirect back to index.html
 if (!currentUser) {
     window.location.href = "index.html";
 }
 
+//function that consumes the API and also disolaying the neccessary details from the API data
 async function showItems() {
     let allItems = await fetch("https://dummyjson.com/products");
     let result = await allItems.json();
@@ -31,7 +35,7 @@ async function showItems() {
                         <p class="title">${element.title}</p>
                         <h5 class="price">${"$"+element.price}</h5>
                     </div>
-                    <div class="btn btn-warning w-100">ADD TO CART</div>
+                    <div class="add-to-cart btn btn-warning w-100">ADD TO CART</div>
                 </div>
                 <div class="position-absolute discount-price rounded bg-warning bg-opacity-75">${"-" + element.discountPercentage + "%"}</div>
             </button>
@@ -39,6 +43,7 @@ async function showItems() {
         `
     });
 }
+//invoking the showItems() function
 showItems();
 
 
